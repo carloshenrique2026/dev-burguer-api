4e3fa99136fbb2bce37fcc5cf0741f85cea248b7
carloshenrique2026 <fariascarlos2346@gmail.com>
2026-02-05 23:30:25 -0300

    Minha atualização do servidor

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -3,7 +3,6 @@ import User from "../models/User.js";
 
 class UserController {
     async store(request, response){
-        console.log('request', response.body)
        const { name, email, password_hash, admin } = request.body;
 
         const user = await User.create({
