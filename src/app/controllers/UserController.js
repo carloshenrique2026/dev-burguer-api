a06c76ba89b7d8136b643b7d152638a3694c2a6f
carloshenrique2026 <fariascarlos2346@gmail.com>
2026-02-06 09:49:01 -0300

    criptografia da senha com Bcrypt

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -1,35 +1,38 @@
 import { v4 } from "uuid";
 import User from "../models/User.js";
 import * as Yup from 'yup';
+import bcrypt from "bcrypt";
 
 class UserController {
-    async store(request, response){
-       const schema = Yup.object({
-        name: Yup.string().required(),
-        email: Yup.string().email().required(),
-        password_hash: Yup.string().min(6).required(),
-        admin: Yup.boolean(),
-       }); 
+    async store(request, response) {
+        const schema = Yup.object({
+            name: Yup.string().required(),
+            email: Yup.string().email().required(),
+            password: Yup.string().min(6).required(),
+            admin: Yup.boolean(),
+        });
 
-       try {
-        schema.validateSync(request.body, { abortEarly: false, strict: true });
-       } catch  (err) {
-        return response.status(400).json({ error: err.errors });
-       }
+        try {
+            schema.validateSync(request.body, { abortEarly: false, strict: true });
+        } catch (err) {
+            return response.status(400).json({ error: err.errors });
+        }
 
-       const { name, email, password_hash, admin } = request.body;
+        const { name, email, password, admin } = request.body;
 
-       const existingUser = await User.findOne({
-        where: {
-            email,
-        },
-       });
+        const existingUser = await User.findOne({
+            where: {
+                email,
+            },
+        });
 
-       if (existingUser) {
-        return response
-        .status(400)
-        .json({ message: 'Email already taken!' });
-       }
+        if (existingUser) {
+            return response
+                .status(400)
+                .json({ message: 'Email already taken!' });
+        }
+
+        const password_hash = await bcrypt.hash(password, 10);
 
         const user = await User.create({
             id: v4(),
